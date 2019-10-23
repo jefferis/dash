2fc3751d5280c465498064d9149e13fb43a905db
Gregory Jefferis <jefferis@gmail.com>
2019-10-23 20:26:56 +0100

    By default attach packages in job
    * by importing the environment and attaching packages to match the original session
      you should be able to run any snippet
    * nb this behaviour is determined by arguments so you could define additional functions with different behaviour

--- R/runSelectionAsJob.R
+++ R/runSelectionAsJob.R
@@ -1,31 +1,45 @@
-runSelectionAsJob <- function() {
+runSelectionAsJob <- function(importEnv=TRUE, attachPackages=TRUE) {
 
   ## Get the document context.
   context <- rstudioapi::getActiveDocumentContext()$selection[[1]]$text
+
+  if(attachPackages) {
+    si=sessionInfo()
+    packages=names(si$otherPkgs)
+  }
 
   ## if not empty selection
   if (!is.null(context) && context != "") {
 
     ## save selection to a temporary file
     tf <- tempfile("dashJob", fileext = ".R")
-    writeLines(context, con = tf, sep = "\n")
+    tfc=file(tf, open='w')
+    if(attachPackages) {
+      writeLines("sapply(", con=tfc, )
+      dput(packages, file = tfc)
+      writeLines(",library, character.only=T)", con=tfc)
+    }
+    writeLines(context, con = tfc, sep = "\n")
+    writeLines("\n", con = tfc)
+    flush(tfc)
+
+    ## delete temporary file
+    on.exit({
+      ## allow time for scripting to start
+      Sys.sleep(5)
+      close(tfc)
+      unlink(tf)
+    }, add=T)
 
     ## run the temp script as a job, returning the
     ## results to the global environment
     .rs.api.runScriptJob(
       path = path.expand(tf),
-      importEnv = TRUE,
+      importEnv = importEnv,
       workingDir = getwd(),
       exportEnv = "R_GlobalEnv"
     )
 
   }
-
-  ## delete temporary file
-  on.exit({
-    ## allow time for scripting to start
-    Sys.sleep(5)
-    unlink(tf)
-  })
 
 }
